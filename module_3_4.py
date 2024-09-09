5e075b6b7cc6cc5cfd8d04965f7d346a324e3b00
Kirill Borisenko <84479339+Kirill-rus@users.noreply.github.com>
2024-09-09 17:12:57 +0300

    Some changes.

--- module_3_4.py
+++ module_3_4.py
@@ -3,12 +3,12 @@
 # Функция поиска однокоренных слов или похожих слов:
 def  single_root_words (root_word, *other_words):
     same_words = []
-    root_word = root_word.lower() # Перевод  проверочного слова в нижний регистр для упрощения поиска.
+    #root_word = root_word.lower() # Перевод  проверочного слова в нижний регистр для упрощения поиска.
     for word in other_words:
-        word = word.lower()  #Перевод слова из списка в нижний регистр для упрощения поиска.
-        if word.find(root_word) >= 0: # Проверка, если слово из списка включает в себя проверочное слово.
+        #word = word.lower()  #Перевод слова из списка в нижний регистр для упрощения поиска.
+        if word.lower().find(root_word.lower()) >= 0: # Проверка, если слово из списка включает в себя проверочное слово.
             same_words.append(word)
-        if root_word.find(word) >= 0: # Проверка, если проверочное слово включает в себя слово из списка.
+        if root_word.lower().find(word.lower()) >= 0: # Проверка, если проверочное слово включает в себя слово из списка.
            same_words.append(word)
 
     return same_words
