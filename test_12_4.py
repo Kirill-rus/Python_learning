0b90201c6475737c3a6af51176188e9238216315
Kirill Borisenko <84479339+Kirill-rus@users.noreply.github.com>
2024-12-13 19:30:55 +0300

    Update test_12_4.py

--- test_12_4.py
+++ test_12_4.py
@@ -1,6 +1,6 @@
 # Выполнение домашнего задания на тему "логирование бегунов".
 
-import runner_12_4
+from runner_12_4 import Runner
 import unittest
 import logging
 
@@ -8,7 +8,7 @@ logging.basicConfig(
     level=logging.DEBUG,  # Уровень логирования
     format='%(asctime)s - %(levelname)s - %(message)s',  # Формат записи
     handlers=[
-        logging.FileHandler('runner_tests.txt', mode='w', encoding='utf-8'),  # Обработчик для записи в файл
+        logging.FileHandler('runner_tests.log', mode='w', encoding='utf-8'),  # Обработчик для записи в файл
         logging.StreamHandler()  # Обработчик для вывода в консоль
     ]
 )
@@ -19,44 +19,43 @@ class RunnerTest(unittest.TestCase):
     #is_frozen = False
 
     #@unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
-    try:
-        def test_walk(self):
+    def test_walk(self):
+        try:
 
-            method = runner_12_4.Runner('method', -10)
+            runner_1 = Runner('runner_1', -10)
             for i in range(0, 10):
-                method.walk()
+                runner_1.walk()
 
-            self.assertEqual(method.distance, 50)
+            self.assertEqual(runner_1.distance, 50)
             logging.info('"test_walk" выполнен успешно')
-    except:
-        logging.warning('Неверная скорость для Runner')
+        except:
+            logging.warning('Неверная скорость для Runner')
 
     #@unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
-    try:
-        def test_run(self):
+    def test_run(self):
+        try:
 
-            method_2 = runner_12_4.Runner('method_2', some_variable)
+            runner_2 = Runner('runner_2', some_variable)
             for j in range(0, 10):
-                method_2.run()
+                runner_2.run()
 
-            self.assertEqual(method_2.distance, 100)
+            self.assertEqual(runner_2.distance, 100)
             logging.info('"test_run" выполнен успешно')
-    except:
-        logging.warning('Неверный тип данных для объекта Runner')
+        except:
+            logging.warning('Неверный тип данных для объекта Runner')
 
 
     #@unittest.skipIf(is_frozen == True, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
 
-        method_3 = runner_12_4.Runner('method_3')
-        method_4 = runner_12_4.Runner('method_4')
+        runner_3 = Runner('runner_3')
+        runner_4 = Runner('runner_4')
 
         for k in range(0, 10):
-            method_3.walk()
-            method_4.run()
+            runner_3.walk()
+            runner_4.run()
 
-            self.assertNotEqual(method_3.distance, method_4.distance)
+            self.assertNotEqual(runner_3.distance, runner_4.distance)
 
 if __name__ == "__main__":
     unittest.main()
-
